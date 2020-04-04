067b211e720a5fa5d852142a78518810c2743676
Saúl Núñez <saul.nunez99@gmail.com>
2020-04-04 13:44:56 -0600

    Add project files.

---
+++ Controllers/GameController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectHamiltonService.Controllers
+{
+    public class GameController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index(string lobbyCode)
+        {
+            return Ok();
+        }
+    }
+}
