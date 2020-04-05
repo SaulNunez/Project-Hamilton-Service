276f8428fdb43a53fb6e4737314262927b06899e
Saúl Núñez <saul.nunez99@gmail.com>
2020-04-05 14:55:55 -0600

    Crear operadores de equivalencia para el objeto de posicion

--- Game/Abstracts/Position.cs
+++ Game/Abstracts/Position.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace ProjectHamiltonService.Game.Abstracts
 {
-    public struct Position
+    public struct Position : IEquatable<Position>
     {
         public int x;
         public int y;
@@ -14,6 +14,32 @@ namespace ProjectHamiltonService.Game.Abstracts
         {
             this.x = x;
             this.y = y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Position position && Equals(position);
+        }
+
+        public bool Equals(Position other)
+        {
+            return x == other.x &&
+                   y == other.y;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(x, y);
+        }
+
+        public static bool operator ==(Position left, Position right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Position left, Position right)
+        {
+            return !(left == right);
         }
     }
 }
