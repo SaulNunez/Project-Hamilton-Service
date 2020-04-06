8eba5f0372fb7adbe623260022af6ec8269664dd
Saúl Núñez <saul.nunez99@gmail.com>
2020-04-06 10:26:36 -0600

    (Broken) Template para el la api del hub

--- Game/ServerHub.cs
+++ Game/ServerHub.cs
@@ -6,8 +6,56 @@ using System.Threading.Tasks;
 
 namespace ProjectHamiltonService.Game
 {
-    public class ServerHub: Hub
+    public class ServerHub : Hub<IGameServer>
     {
+        public async Task<bool> GoToLeft()
+        {
 
+        }
+
+        public async Task<bool> GoToRight()
+        {
+
+        }
+
+        public async Task<bool> GoToBottom()
+        {
+
+        }
+
+        public async Task<bool> GoToTop()
+        {
+
+        }
+
+        public async Task UseItem()
+        {
+
+        }
+
+        public async Task<List<Items>> GetItems()
+        {
+
+        }
+
+        public async Task SendPuzzle()
+        {
+
+        }
+
+        public async Task EnterLobby(string code)
+        {
+
+        }
+
+        public async Task<Character> GetAvailableCharacters()
+        {
+
+        }
+
+        public async Task GetStatsForCharacter(string? character = null)
+        {
+
+        }
     }
 }
