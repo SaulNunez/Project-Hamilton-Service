8a56f405abdf3e5415c8f4658f69ad99c8812a18
Saúl Núñez <saul.nunez99@gmail.com>
2020-04-09 02:49:27 -0600

    Agregar items

--- Game/Item.cs
+++ Game/Item.cs
@@ -7,6 +7,56 @@ namespace LaCasaDelTerror.Models
 {
     public class Items
     {
+        public static Dictionary<string, Items> items = new Dictionary<string, Items>();
+        
+        static Items()
+        {
+            items.Add("revolver", new Items
+            {
+                needsThrow=true,
+                statEffects=new Stats
+                {
+                    physical=-1
+                },
+                singleUse=true
+            });
+
+            items.Add("magic_stone", new Items
+            {
+                singleUse=true,
+                needsThrow=true
+            });
+            items.Add("small_totem", new Items { 
+                statEffects=new Stats
+                {
+                    sanity=-1
+                }
+            });
+            items.Add("sacrificial_dagger", new Items
+            {
+                needsThrow=true,
+                //TODO: Agregar informacion de custom throw
+                statEffects = new Stats{
+                
+                }
+            });
+            items.Add("buttler_key", new Items
+            {
+                specialItem=SpecialItems.KEY,
+                singleUse=true
+            });
+            items.Add("adrenaline_shot", new Items
+            {
+                singleUse = true,
+                statEffects = new Stats
+                {
+                    sanity = -1,
+                    physical = 1,
+                    balls = 2
+                }
+            });
+        }
+
         public enum SpecialItems
         {
             KEY,
@@ -14,6 +64,7 @@ namespace LaCasaDelTerror.Models
             NONE
         }
 
+        public bool needsThrow = false;
         public Stats? statEffects;
         public bool singleUse = false;
         public bool affectsOtherPlayer =  false;
