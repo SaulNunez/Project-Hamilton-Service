86f85cd171bf0d6add0c51cc5579bd25b4225367
Saul Nuñez <saul.nunez99@gmail.com>
2020-07-27 00:47:20 -0600

    Crear un constructor para evitar errores al intentar crear el contexto de base de datos

--- Models/GameContext.cs
+++ Models/GameContext.cs
@@ -12,5 +12,9 @@ namespace ProjectHamiltonService.Models
         public DbSet<Lobbies> Lobbies { get; set; }
         public DbSet<Players> Players { get; set; }
         public DbSet<Items> Items { get; set; }
+
+        public GameContext(DbContextOptions<GameContext> options)
+    : base(options)
+        { }
     }
 }
