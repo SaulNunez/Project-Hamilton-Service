29ed70958b7e3ff29e9758d569101314135e2af5
Saul Nuñez <saul.nunez99@gmail.com>
2020-10-04 00:01:15 -0600

    Usar propiedades para objetos de datos. Agregar resultado available characters a un objecto. Actualizar cliente para soportar la información de esta manera.

---
+++ Game/ResponseModels/CharacterData.cs
@@ -0,0 +1,25 @@
+﻿using LaCasaDelTerror.Assets;
+using LaCasaDelTerror.Assets.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectHamiltonService.Game.ResponseModels
+{
+    public class CharacterData
+    {
+        public string Name { get; set; }
+        public Stats Stats { get; set; }
+        public string Description { get; set; }
+        public string Id { get; set; }
+
+        public CharacterData(Character character)
+        {
+            Name = character.name;
+            Stats = character.stats;
+            Description = character.description;
+            Id = character.id;
+        }
+    }
+}
