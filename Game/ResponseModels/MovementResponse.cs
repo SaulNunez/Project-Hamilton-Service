77e10aecef67eb4bb03c5efb0b29cba8393a16b4
Saul Nuñez <saul.nunez99@gmail.com>
2020-10-05 20:04:22 -0600

    Agregar funciones del cliente segun documentación

---
+++ Game/ResponseModels/MovementResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectHamiltonService.Game.ResponseModels
+{
+    public class MovementResponse
+    {
+        public string MovementDirection { get; set; }
+    }
+}
